7d5330607f34f3d9b8a0c20ccc846600f583743d
chaseleif <chaseleif@icloud.com>
2024-04-29 00:03:33 -0500

    add more options to driver, both functions can splitimg, pause, show, save/etc. change showimg scaling

--- driver.py
+++ driver.py
@@ -15,8 +15,18 @@ if __name__ == '__main__':
                       help='show this help message.')
   parser.add_argument('-s', '--stellarium', action='store_true',
                       help='Run Stellarium to generate images')
-  parser.add_argument('-t', '--test', action='store_true',
-                      help='Run under construction test')
+  parser.add_argument('-d', '--detect', action='store_true',
+                      help='Run satellite detection on input')
+  parser.add_argument('--show', action='store_true', default=False,
+                      help='Show output images')
+  parser.add_argument('--dontsave', action='store_true', default=False,
+                      help='Don\'t save output images')
+  parser.add_argument('--pause', action='store_true',
+                      help='Pause and prompt to continue after each frame')
+  parser.add_argument('--split', action='store_true',
+                      help='Produce split image output showing image stages')
+  parser.add_argument('--truth', action='store_true',
+                      help='Produce truth image output and create csv output')
   args = vars(parser.parse_args())
   if 'help' in args:
     parser.print_help()
@@ -26,24 +36,28 @@ if __name__ == '__main__':
     ret = run_stellarium(basedir)
     if ret == 0: print('Stellarium completed successfully')
     else: print('Stellarium exited with code', ret)
-  if 'test' in args:
+  if 'detect' not in args and ('split' in args or 'truth' in args):
+    args['detect'] = True
+  if 'detect' in args:
     imgpath = os.path.join(basedir, 'data')
     if not os.path.isdir(imgpath):
       print(f'Data directory {imgpath} doesn\'t exist')
       sys.exit(1)
+    pause = 'pause' in args
+    truth = 'truth' in args
+    args = {'splitimg':args['split'] if 'split' in args else False,
+            'show':args['show'],
+            'save':not args['dontsave']}
+    if truth: print('image,num_sats,tp,fp,missed')
     imgnum = 1
     imgname = os.path.join(imgpath, f'img{imgnum:03d}.png')
-    cont = True
-    # Whether we're making a csv
-    csv = True
-    if csv: print('image,num_sats,tp,fp,missed')
-    while cont and os.path.isfile(imgname):
-      if csv:
-        getStats(imgname)
+    while os.path.isfile(imgname):
+      if truth:
+        getStats(imgname, **args)
       else:
-        markSatellites(imgname)
+        markSatellites(imgname, **args)
+      if pause and not input('Continue? ').lower().startswith('y'):
+        break
       imgnum += 1
       imgname = os.path.join(imgpath, f'img{imgnum:03d}.png')
-      #cont = input('Continue? [y/N]: ')
-      #cont = cont.lower().startswith('y')
 
--- starcv/contour.py
+++ starcv/contour.py
@@ -6,30 +6,24 @@ import numpy as np
 from tkinter import Tk
 
 loadimg = lambda path: cv.imread(path) #, cv.IMREAD_GRAYSCALE)
-upscaleimg = lambda img: np.array(img*255,dtype=np.uint8)
-downscaleimg = lambda img: np.array(img/255,dtype=np.float32)
-gaussian = lambda img: cv.GaussianBlur(img,(5,5),0)
 
 root = Tk()
 root.withdraw()
-screenx, screeny = root.winfo_screenwidth(), root.winfo_screenheight()
+screenx, screeny = root.winfo_screenwidth()-100, root.winfo_screenheight()-100
 del root
 
 def showImg(img, title='Image'):
-  if showImg.scale is None:
-    scale = min(1,min(img.shape[0]/screeny, img.shape[1]/screenx))
-    showImg.scale = (img.shape[0]*scale,img.shape[1]*scale) if scale < 1 \
-                                                            else False
   cv.namedWindow(title)
-  if isinstance(showImg.scale, tuple):
-    scaled = cv.resize(img, showImg.scale)
+  scale = min(screeny/img.shape[0], screenx/img.shape[1])
+  if scale < 1:
+    scale = (int(img.shape[1]*scale),int(img.shape[0]*scale))
+    scaled = cv.resize(img, scale)
     cv.imshow(title, scaled)
   else:
     cv.imshow(title, img)
-  cv.moveWindow(title, 1, 1)
+  cv.moveWindow(title, 20, 20)
   cv.waitKey()
   cv.destroyAllWindows()
-showImg.scale = None
 
 # splitimg for the 4x4 phases of image splitting/saving
 # truth for getstats
@@ -76,31 +70,35 @@ def processImgStream(img, splitimg=False, truth=None):
     for contour in contours:
       x,y,w,h = cv.boundingRect(contour)
       img = cv.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
-    return img, tp, fp, missed
-  for contour in contours:
-    x,y,w,h = cv.boundingRect(contour)
-    img = cv.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+    #return img, tp, fp, missed
+  else:
+    for contour in contours:
+      x,y,w,h = cv.boundingRect(contour)
+      img = cv.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
   if not splitimg:
+    if truth is not None: return img, tp, fp, missed
     return img
   bottom = cv.cvtColor(fg_mask, cv.COLOR_GRAY2RGB)[400:800,400:800]
   bottom = np.concatenate((bottom,img[400:800,400:800]), axis=1)
   saveimg = np.concatenate((saveimg,bottom), axis=0)
   saveimg = cv.line(saveimg, (0,399),(799,399), (102,255,102), 5)
   saveimg = cv.line(saveimg, (399,0),(399,799), (102,255,102), 5)
+  if truth is not None: return saveimg, tp, fp, missed
   return saveimg
 processImgStream.backsub = cv.createBackgroundSubtractorMOG2()
 #processImgStream.backsub = cv.createBackgroundSubtractorKNN()
 
-def markSatellites(imgpath):
+def markSatellites(imgpath, splitimg=False, show=False, save=True):
   img = loadimg(imgpath)
-  #showImg(img, 'Input')
-  marked = processImgStream(img)
-  #showImg(marked, 'Marked')
-  outname = os.path.join('output',imgpath.split(os.path.sep)[-1])
-  cv.imwrite(outname, marked)
+  if show and not splitimg: showImg(img, 'Input')
+  marked = processImgStream(img, splitimg=splitimg)
+  if show: showImg(marked, 'Marked')
+  if save:
+    outname = os.path.join('output',imgpath.split(os.path.sep)[-1])
+    cv.imwrite(outname, marked)
 
 # can print field name/val or csv-style
-def getStats(imgpath):
+def getStats(imgpath, splitimg=False, show=False, save=True):
   img = cv.imread(imgpath, cv.IMREAD_GRAYSCALE)
   img[img<100] = 0
   # this is img[85:100,62:75] from data/img001.png, the 32fps 1 min data
@@ -131,16 +129,18 @@ def getStats(imgpath):
   numsats = 0
   for pt in zip(*loc[::-1]):
     # this region is black, increment the number of satellites
-    if np.sum(truth[pt[1]:pt[1]+h,pt[0]:pt[0]+w]) == 0:
+    if np.max(truth[pt[1]:pt[1]+h,pt[0]:pt[0]+w]) == 0:
       numsats += 1
     # draw the rectangle
     cv.rectangle(truth, pt, (pt[0] + w, pt[1] + h), 255, -1)
   # load the color image
   img = loadimg(imgpath)
+  if show and not splitimg: showImg(img, 'Input')
   # processImgStream with the "truth"
-  img, tp, fp, missed = processImgStream(img, truth=truth)
-  outname = os.path.join('output',imgpath.split(os.path.sep)[-1])
-  cv.imwrite(outname, img)
-  #print(f'{imgpath.split(os.path.sep)[-1]}: {numsats=} {tp=} {fp=} {missed=}')
+  img, tp, fp, missed = processImgStream(img, splitimg=splitimg, truth=truth)
+  if show: showImg(img, 'Marked')
+  if save:
+    outname = os.path.join('output',imgpath.split(os.path.sep)[-1])
+    cv.imwrite(outname, img)
   print(f'{imgpath.split(os.path.sep)[-1]},{numsats},{tp},{fp},{missed}')
 
